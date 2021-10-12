0bce60c4731760c1786148c1ba259a37704406a0
fruskate <fruskate@gmail.com>
2021-10-13 00:28:41 +0600

    Используем в импорте только те книги, где есть название

--- plugins/frukt/books/imports/BooksImport.php
+++ plugins/frukt/books/imports/BooksImport.php
@@ -9,30 +9,32 @@ class BooksImport implements ToModel
 {
     public function model(array $row)
     {
-        if ($row[1]) {
-            $author = Author::where('name', $row[1])->firstOrCreate(['name' => $row[1]]);
-        }
-
-        $book = new Book();
-        $book->import_id = $row[0];
-        $book->author_id = $row[1]? $author->id : null;
-        $book->title = $row[2];
-        $book->year = $row[5]? : null;
-        $book->age = $row[12]? : null;
-        $book->save();
-
-        if ($row[6]) {
-            $languages = explode(' , ', $row[6]);
-
-            foreach ($languages as $language) {
-                $modelLanguage = Language::where('name_short', $language)->firstOrCreate(['name_short' => $language]);
-
-                $book->languages()->attach($modelLanguage->id);
+        if ($row[2]) { // Если у книги есть название - импортируем её
+            if ($row[1]) {
+                $author = Author::where('name', $row[1])->firstOrCreate(['name' => $row[1]]);
             }
+
+            $book = new Book();
+            $book->import_id = $row[0];
+            $book->author_id = $row[1]? $author->id : null;
+            $book->title = $row[2];
+            $book->year = $row[5]? : null;
+            $book->age = $row[12]? : null;
+            $book->save();
+
+            if ($row[6]) {
+                $languages = explode(' , ', $row[6]);
+
+                foreach ($languages as $language) {
+                    $modelLanguage = Language::where('name_short', $language)->firstOrCreate(['name_short' => $language]);
+
+                    $book->languages()->attach($modelLanguage->id);
+                }
+            }
+
+
+
+            return $book;
         }
-
-
-
-        return $book;
     }
 }
